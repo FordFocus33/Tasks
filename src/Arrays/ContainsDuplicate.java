f77b82a223f2fe677658aad8cacff0c72673d6f4
Ivan Burov <i.burov@krtech.ru>
2025-07-09 17:54:33 +0300

    new algos

---
+++ src/Arrays/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,1};
+        System.out.println((Arrays.toString(new boolean[]{containsDuplicate(nums)})));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i == nums.length - 1) {
+                System.out.println(i);
+                return set.contains(nums[i]);
+            }
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
