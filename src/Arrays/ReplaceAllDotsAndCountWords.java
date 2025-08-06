54c6577c34f7f5b0acfa78ee90d82a2e419bd251
Ivan Burov <i.burov@krtech.ru>
2025-08-06 15:53:34 +0300

    +1 algo

---
+++ src/Arrays/ReplaceAllDotsAndCountWords.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+import java.util.HashMap;
+
+public class ReplaceAllDotsAndCountWords {
+    public static void main(String[] args) {
+        String s = "Привет, как деЛА, мой доБрый, мой милый друг? дела привет!";
+        String[] arr = s.split(" ");
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String str : arr) {
+            str = str.toLowerCase().replaceAll("[,.?!]", "");
+            map.merge(str, 1, Integer::sum);
+        }
+        System.out.println(map);
+    }
+}
