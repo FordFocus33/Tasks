c459f63511082581be1c93f9e99ab4037e393a34
Ivan Burov <i.burov@krtech.ru>
2025-08-06 15:54:45 +0300

    +1 algo

--- src/Arrays/ReplaceAllDotsAndCountWords.java
+++ src/Arrays/ReplaceAllDotsAndCountWords.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 public class ReplaceAllDotsAndCountWords {
     public static void main(String[] args) {
+        //Без привязки к регистру, не учитывать знаки препинания, подсчитать кол-во повторяющихся слов.
         String s = "Привет, как деЛА, мой доБрый, мой милый друг? дела привет!";
         String[] arr = s.split(" ");
         HashMap<String, Integer> map = new HashMap<>();
