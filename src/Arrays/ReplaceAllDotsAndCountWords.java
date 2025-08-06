2182565dafc35ac06a596dc97638a696f026ecfe
Ivan Burov <i.burov@krtech.ru>
2025-08-06 16:15:09 +0300

    +1 algo

--- src/Arrays/ReplaceAllDotsAndCountWords.java
+++ src/Arrays/ReplaceAllDotsAndCountWords.java
@@ -1,11 +1,19 @@
 package Arrays;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ReplaceAllDotsAndCountWords {
     public static void main(String[] args) {
         //Без привязки к регистру, не учитывать знаки препинания, подсчитать кол-во повторяющихся слов.
         String s = "Привет, как деЛА, мой доБрый, мой милый друг? дела привет!";
+        firstSolution(s);
+        oneLineSolution(s);
+    }
+
+    public static void firstSolution(String s) {
         String[] arr = s.split(" ");
         HashMap<String, Integer> map = new HashMap<>();
 
@@ -13,6 +21,12 @@ public class ReplaceAllDotsAndCountWords {
             str = str.toLowerCase().replaceAll("[,.?!]", "");
             map.merge(str, 1, Integer::sum);
         }
+        System.out.println(map);
+    }
+
+    public static void oneLineSolution(String s) {
+        Map<String, Integer> map = Arrays.asList(s.toLowerCase().replaceAll("[,.!?]", "").split(" "))
+                .stream().collect(Collectors.toMap(wrd -> wrd, wrd -> 1, Integer::sum));
         System.out.println(map);
     }
 }
