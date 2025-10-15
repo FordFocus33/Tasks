a552328ded6325bddb5766420215f02692d9cfbc
Ivan Burov <i.burov@krtech.ru>
2025-10-15 14:54:28 +0300

    + algo

--- src/leetcode/NumbersOfIslands.java
+++ src/leetcode/NumbersOfIslands.java
@@ -2,44 +2,45 @@ package leetcode;
 
 public class NumbersOfIslands {
     public static void main(String[] args) {
-//        char[][] grid = {      {'1','1','1','1','0'},
-//                              {'1','1','0','0','0'},
-//                              {'0','0','0','1','0'},
-//                              {'1','0','1','0','0'}};
+        char[][] grid = {      {'1','1','1','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','1','0'},
+                {'1','0','1','0','0'}};
 
 //        char[][] grid = {       {'1','1','1'},
 //                                {'0','1','0'},
 //                                {'1','1','1'}};
+//
+//        char[][] grid = {       {'1','0','1','1','1'},
+//                {'1','0','1','0','1'},
+//                {'1','1','1','0','1'}};
 
-        char[][] grid = {       {'1','0','1','1','1'},
-                                {'1','0','1','0','1'},
-                                {'1','1','1','0','1'}};
-        hello(grid);
+        int x = grid[0].length;
+        int y = grid.length;
+
+        hello(grid, x, y);
     }
 
-    public static void hello(char[][] grid) {
-      int isLand = 0;
-      int x = grid[0].length;
-      int y = grid.length;
-
+    public static void hello(char[][] mas, int x, int y) {
+        int result = 0;
         for (int i = 0; i < y; i++) {
             for (int j = 0; j < x; j++) {
-                if (grid[i][j] == '1') {
-                    isLand++;
-                    changeNeighboursToZero(grid, i, j, x, y);
+                if (mas[i][j] == '1') {
+                    result++;
+                    transformer(mas, x, y, i, j);
                 }
             }
         }
-
-        System.out.println(isLand);
+        System.out.println(result);
     }
-    public static void changeNeighboursToZero(char[][] grid, int a, int b, int x, int y) {
-                if (grid[a][b] == '1') {
-                    grid[a][b] = '0';
-                    if (y-1 != a) changeNeighboursToZero(grid, a+1, b, x, y);
-                    if (a-1 >= 0) changeNeighboursToZero(grid, a-1, b, x, y);
-                    if (x-1 != b) changeNeighboursToZero(grid, a, b+1, x, y);
-                    if (b-1 >= 0) changeNeighboursToZero(grid, a, b-1, x, y);
-                }
+
+    public static void transformer(char[][] mas, int x, int y, int i, int j) {
+        if (mas[i][j] == '1') {
+            mas[i][j] = '0';
+            if (i + 1 < y) transformer(mas, x, y, i + 1, j);
+            if (j + 1 < x) transformer(mas, x, y, i, j + 1);
+            if (i - 1 >= 0) transformer(mas, x, y, i - 1, j);
+            if (j - 1 >= 0) transformer(mas, x, y, i, j - 1);
+        }
     }
 }
